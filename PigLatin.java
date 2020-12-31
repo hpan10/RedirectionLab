406b1b7d9d110af235dbb9f5f4aa1675625bdddc
Harry Pan <hpan10@stuy.edu>
2020-12-31 11:20:33 -0500

    I've completed and tested PigLatinBest and it works!

--- PigLatin.java
+++ PigLatin.java
@@ -10,11 +10,22 @@ public class PigLatin{
 
   public static String pigLatin(String s){
     String digraphs = "bl,br,ch,ck,cl,cr,dr,fl,fr,gh,gl,gr,ng,ph,pl,pr,qu,sc,sh,sk,sl,sm,sn,sp,st,sw,th,tr,tw,wh,wr";
+    s.toLowerCase();
     if (s.length() > 1 && digraphs.contains(s.substring(0, 2))) return s.substring(2) + s.substring(0, 2) + "ay";
     else return pigLatinSimple(s);
   }
 
+  public static String pigLatinBest(String s){
+    if (Character.isLetter(s.charAt(0))){
+      if (Character.isLetter(s.charAt(s.length() - 1))) return pigLatin(s);
+      else return pigLatin(s.substring(0, s.length() - 1)) + s.charAt(s.length() - 1);
+    }
+    else return s;
+  }
+
   public static void main(String[] args) {
-    System.out.println(pigLatin(args[0]));
+    for (int i = 0; i < args.length; i++){
+      System.out.println(pigLatinBest(args[i]));
+    }
   }
 }
