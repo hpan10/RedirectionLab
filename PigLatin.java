771906a0bd3424ede4d6f55b33e6c9843c5b0205
Harry Pan <hpan10@stuy.edu>
2020-12-31 11:43:38 -0500

    I've tested PigLatin with sonnet.txt and compared it to expectedOutput and it works!

--- PigLatin.java
+++ PigLatin.java
@@ -1,8 +1,9 @@
+import java.util.Scanner;
 public class PigLatin{
 
   public static String pigLatinSimple(String s){
     String vowels = "aeiou";
-    s.toLowerCase();
+    s = s.toLowerCase();
     if (vowels.contains("" + s.charAt(0))) return s + "hay";
     if (s.length() > 1) return s.substring(1) + s.charAt(0) + "ay";
     return s + "ay";
@@ -10,12 +11,13 @@ public class PigLatin{
 
   public static String pigLatin(String s){
     String digraphs = "bl,br,ch,ck,cl,cr,dr,fl,fr,gh,gl,gr,ng,ph,pl,pr,qu,sc,sh,sk,sl,sm,sn,sp,st,sw,th,tr,tw,wh,wr";
-    s.toLowerCase();
+    s = s.toLowerCase();
     if (s.length() > 1 && digraphs.contains(s.substring(0, 2))) return s.substring(2) + s.substring(0, 2) + "ay";
     else return pigLatinSimple(s);
   }
 
   public static String pigLatinBest(String s){
+    s = s.toLowerCase();
     if (Character.isLetter(s.charAt(0))){
       if (Character.isLetter(s.charAt(s.length() - 1))) return pigLatin(s);
       else return pigLatin(s.substring(0, s.length() - 1)) + s.charAt(s.length() - 1);
@@ -24,8 +26,14 @@ public class PigLatin{
   }
 
   public static void main(String[] args) {
-    for (int i = 0; i < args.length; i++){
-      System.out.println(pigLatinBest(args[i]));
+    Scanner file = new Scanner(System.in);
+    while (file.hasNext()){
+      Scanner line = new Scanner(file.nextLine());
+      while (line.hasNext()){
+        System.out.print(pigLatinBest(line.next()));
+        if (line.hasNext()) System.out.print(' ');
+      }
+      if (file.hasNext()) System.out.println("");
     }
   }
 }
